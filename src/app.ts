20851023442f9b75540f71333e2a0d1e1efe68fe
alexandr.nikiforov <nikiforov.san.sanich@gmail.com>
2023-02-27 19:01:08 +0400

    Implement drag-and-drop project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Some text');
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
+
+
+namespace App {
+ 
+  new ProjectInput();
+  new ProjectList("active");
+  new ProjectList("finished");
+}
